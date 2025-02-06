e4eb2a47e60b3d629a2d6096b9b823a07e1f213e
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2025-02-06 22:09:23 +0100

    dssd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  async headers() {
+    return [
+      {
+        // Pot do API endpointa
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // V produkciji specificiraj domeno
+          { key: "Access-Control-Allow-Methods", value: "POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
