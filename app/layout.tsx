dc0473c08c9c6559451741e1cadcab7ee32cc533
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2025-02-07 15:37:17 +0100

    gddfgdfgdfg

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Teko } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Montserrat
 const montserrat = Montserrat({
@@ -36,6 +37,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
         className={`${teko.variable} ${montserrat.variable} antialiased overflow-x-hidden`}
       >
         <Analytics/>
+        <SpeedInsights/>
         {children}
       </body>
     </html>
