0e6068dac1cef6fdfc787111cb8f63757410f192
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2025-02-07 15:31:49 +0100

    vbvb

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  async headers() {
-    return [
-      {
-        // Pot do API endpointa
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // V produkciji specificiraj domeno
-          { key: "Access-Control-Allow-Methods", value: "POST,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-        ]
-      }
-    ]
+  output: 'export'
   }
-};
 
 export default nextConfig;
